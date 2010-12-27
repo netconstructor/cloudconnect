770fd2a78268f372b7644e123aafb34368b163cd
Dirk Olmes <dirk.olmes@mulesoft.com>
2010-12-27 17:31:37 +0100

    add an empty cloud connector class

---
+++ mule-wsdl-cloud-connector-archetype/src/main/resources/archetype-resources/src/main/java/__cloudService__CloudConnector.java
@@ -0,0 +1,21 @@
+#set($D = '$')
+/*
+ * ${D}Id:${D}
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package ${package}
+
+public class ${cloudService}CloudConnector
+{
+    /*
+     * TODO
+     *     - add getters/setters for configuration settings
+     *     - add code that interacts with the cloud service
+     */
+}
