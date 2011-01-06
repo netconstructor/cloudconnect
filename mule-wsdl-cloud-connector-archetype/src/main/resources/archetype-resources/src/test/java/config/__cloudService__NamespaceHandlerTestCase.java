7a02b8d9ccdc1662c0870ec00098fe5a65046097
Dirk Olmes <dirk.olmes@mulesoft.com>
2011-01-06 19:25:51 +0100

    remove license headers

--- mule-wsdl-cloud-connector-archetype/src/main/resources/archetype-resources/src/test/java/config/__cloudService__NamespaceHandlerTestCase.java
+++ mule-wsdl-cloud-connector-archetype/src/main/resources/archetype-resources/src/test/java/config/__cloudService__NamespaceHandlerTestCase.java
@@ -1,14 +1,4 @@
 #set($D = '$')
-/*
- * ${D}Id${D}
- * --------------------------------------------------------------------------------------
- * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
- *
- * The software in this package is published under the terms of the CPAL v1.0
- * license, a copy of which has been included with this distribution in the
- * LICENSE.txt file.
- */
-
 package ${package}.config;
 
 import org.mule.tck.FunctionalTestCase;
