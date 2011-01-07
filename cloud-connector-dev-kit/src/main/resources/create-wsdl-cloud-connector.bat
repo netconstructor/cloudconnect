5adf1a0fd309ca47307bdc02dd8c8a27cc9fd843
Dirk Olmes <dirk.olmes@mulesoft.com>
2011-01-07 13:51:14 +0100

    move the scripts to the toplevel folder, only the groovy script resides in the bin folder. Adjust the paths in both scripts

--- cloud-connector-dev-kit/src/main/resources/create-wsdl-cloud-connector.bat
+++ cloud-connector-dev-kit/src/main/resources/create-wsdl-cloud-connector.bat
@@ -8,10 +8,10 @@ setlocal enabledelayedexpansion
 set _REALPATH=%~dp0
 
 :: Use the path that was used to launch this script to determine the location
-:: of DEVKIT_HOME. Since this script resides in the bin folder of the 
-:: distribution, we need to cut off the last 5 chars (\bin\) from the real
+:: of DEVKIT_HOME. Since this script resides in the toplevel folder of the
+:: distribution, we need to cut off the last chars (\) from the real
 :: path to determine the proper DEVKIT_HOME
-set DEVKIT_HOME=%_REALPATH:~0,-5%
+set DEVKIT_HOME=%_REALPATH:~0,-1%
 
 :: dynamically evaluate the contents of the classpath
 set CP=
@@ -24,7 +24,7 @@ for /L %%v in (0,1,1) do set ARCHETYPE_SCRIPT_FILE=!ARCHETYPE_SCRIPT_FILE!!Rando
 set ARCHETYPE_SCRIPT_FILE=%TEMP%\%ARCHETYPE_SCRIPT_FILE%.bat
 
 :: ask some questions and generate the scipt file that invokes the archetype
-java -cp %CP% org.codehaus.groovy.tools.GroovyStarter --main groovy.ui.GroovyMain %DEVKIT_HOME%/bin/CreateWSDLArchetypeProperties.groovy %ARCHETYPE_SCRIPT_FILE%
+java -cp %CP% org.codehaus.groovy.tools.GroovyStarter --main groovy.ui.GroovyMain %DEVKIT_HOME%\bin\CreateWSDLArchetypeProperties.groovy %ARCHETYPE_SCRIPT_FILE%
 
 :: now run the archetype
 call %ARCHETYPE_SCRIPT_FILE%
