a148ddd109c549e497732a0a2d6e3e263ba4c564
Dirk Olmes <dirk.olmes@mulesoft.com>
2011-01-08 03:58:44 +0100

    this archetype is not for WSDL based project any more, rename it to something more generic

---
+++ mule-cloud-connector-archetype/src/main/resources/archetype-resources/src/main/java/__cloudService__CloudConnector.java
@@ -0,0 +1,11 @@
+#set($D = '$')
+package ${package};
+
+public class ${cloudService}CloudConnector
+{
+    /*
+     * TODO
+     *     - add getters/setters for configuration settings
+     *     - add code that interacts with the cloud service
+     */
+}
---
+++ mule-cloud-connector-archetype/src/main/resources/archetype-resources/src/test/java/config/__cloudService__NamespaceHandlerTestCase.java
@@ -0,0 +1,39 @@
+#set($D = '$')
+package ${package}.config;
+
+import org.mule.construct.SimpleFlowConstruct;
+import org.mule.tck.FunctionalTestCase;
+
+public class ${cloudService}NamespaceHandlerTestCase extends FunctionalTestCase
+{
+    @Override
+    protected String getConfigResources()
+    {
+        return "${cloudServiceLower}-namespace-config.xml";
+    }
+
+    public void testSendMessageToFlow() throws Exception
+    {
+        /*
+
+        To test your flow directly (i.e. without any inbound endpoints, declare a flow in
+        ${cloudServiceLower}-namespace-config.xml and put the element from your
+        cloud connector's namespace that you want to test into it.
+        A proper example was put into ${cloudServiceLower}-namespace-config.xml
+
+        Now you can send data to your test flow from the unit test:
+
+        String payload = <your input to the flow here>;
+        SimpleFlowConstruct flow = lookupFlowConstruct("theFlow");
+        MuleEvent event = getTestEvent(payload);
+        MuleEvent responseEvent = flowConstruct.process(event);
+        assertEquals(<expected test output>, responseEvent.getMessage().getPayloadAsString());
+
+        */
+    }
+
+    private SimpleFlowConstruct lookupFlowConstruct(String name)
+    {
+        return (SimpleFlowConstruct) muleContext.getRegistry().lookupFlowConstruct(name);
+    }
+}
