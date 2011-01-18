8d8e848d643cb219914243c6520e7cbc9a877afd
Emiliano Lesende <emiliano.lesende@mulesource.com>
2011-01-18 18:02:05 -0300

    Replaced Google's Java Parser with QDox. Removed several classes in the process. Also replaced custom mocked classes with EasyMock also removing several classes

---
+++ mule-cloud-connector-generator/src/test/java/org/mule/tools/cc/generator/EasyMockHelper.java
@@ -0,0 +1,11 @@
+package org.mule.tools.cc.generator;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Emiliano
+ * Date: 1/18/11
+ * Time: 5:53 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class EasyMockHelper {
+}
