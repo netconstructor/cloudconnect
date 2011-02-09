3ec4a3a75e3f69ce6a2c5fa97ec2ec1f18292907
Emiliano Lesende <emiliano.lesende@gmail.com>
2011-02-09 11:40:16 -0300

    Organized imports

--- mule-cloud-connector-maven-plugin/src/main/java/org/mule/tools/cloudconnect/SchemaGenerateMojo.java
+++ mule-cloud-connector-maven-plugin/src/main/java/org/mule/tools/cloudconnect/SchemaGenerateMojo.java
@@ -26,7 +26,6 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.List;
 
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
@@ -38,6 +37,7 @@ import org.codehaus.plexus.util.IOUtil;
  */
 public class SchemaGenerateMojo extends AbstractConnectorMojo
 {
+
     public void execute() throws MojoExecutionException, MojoFailureException
     {
         createAndAttachGeneratedSourcesDirectory();
