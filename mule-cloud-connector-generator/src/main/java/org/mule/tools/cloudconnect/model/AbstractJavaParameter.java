60058da14d85f7454811c384de439372d22dcca7
Pablo Martin Grigolatto <pablogrigo@gmail.com>
2011-03-24 19:31:39 -0300

    Ability to specify alternative name for @Property and @Parameter Based on commit c853c404d9d7198b41af7e2391ff0f962673fe61

--- mule-cloud-connector-generator/src/main/java/org/mule/tools/cloudconnect/model/AbstractJavaParameter.java
+++ mule-cloud-connector-generator/src/main/java/org/mule/tools/cloudconnect/model/AbstractJavaParameter.java
@@ -20,6 +20,21 @@ package org.mule.tools.cloudconnect.model;
 public abstract class AbstractJavaParameter extends AbstractJavaElement implements JavaParameter
 {
 
+    @Override
+    public String getElementName()
+    {
+        for (JavaAnnotation annotation : getAnnotations())
+        {
+            if (annotation.getType().equals("org.mule.tools.cloudconnect.annotations.Parameter") &&
+                annotation.getNamedParameter("name") != null)
+            {
+                return ((String) annotation.getNamedParameter("name")).replace("\"", "");
+            }
+        }
+
+        return super.getElementName();
+    }
+
     public boolean isOptional()
     {
         for (JavaAnnotation annotation : getAnnotations())
